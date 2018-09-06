c65a9668eef499912155fcb5929eb5df5c0a8639
Joseph McDade <JMcDade42@gmail.com>
2018-09-06 17:23:10 -0500

    Updated comments. Changed print statements to use newer format style. Changed logic on image exists from not exists to does exists

--- olympiya-site-scrape.py
+++ olympiya-site-scrape.py
@@ -3,42 +3,42 @@ from urllib.request import urlopen as uReq
 from urllib.request import urlretrieve as uRet
 import os
 
-# Variables
-SITE = "http://olympiya.com/gallary"
-
 # Get html from site
-url = SITE
+url = "http://olympiya.com/gallary"
 uClient = uReq(url)
 page_html = uClient.read()
 uClient.close()
-
 # Parse html
 page_soup = soup(page_html, "html.parser")
+
 # Get gallery pages
 galleries = page_soup.findAll("div",{"class":"vc-hoverbox-back-inner"})
+
 # Loop through each gallery
 for gallery in galleries:
 	gallery_url = gallery.p.a["href"]
-	# Make directory
+	# Make gallery directory
 	dest = gallery_url.split("/")[-2]
 	if not os.path.exists(dest):
 		os.makedirs(dest)
-	# Get new page source
+	# Get gallery page source
 	uClient = uReq(gallery_url)
 	gallery_html = uClient.read()
 	uClient.close()
-	# Parse gallery
+	# Parse gallery html
 	gallery_soup = soup(gallery_html, "html.parser")
-	# Get each picture
+	# Get urls for images
 	images = gallery_soup.findAll("div",{"class":"image_wrapper"})
 	# Download each image
 	for image in images:
 		download = image.img["src"]
 		filename = dest+"/"+download.split("/")[-1]
-		if not os.path.exists(filename):
-			print("Downloading: %a" % filename)
-			uRet(download, filename)
+		# Check if already downloaded
+		if os.path.exists(filename):
+			print("File {} already exists".format(filename))
 		else:
-			print("File %a already exists" % filename)
+			print("Downloading: {}".format(filename))
+			uRet(download, filename)
+	print("Finished downloading {} images from {} gallery".format(len(images),dest))
 # Finished
-print("Finished scraping")
+print("Finished scraping site")
